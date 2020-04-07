d024f87fda89e52bc4c461ca96d3cae61f913ed2
Laxman Bhattarai <erluxman@gmail.com>
2020-04-07 12:59:47 +0545

    some improvement in food details page

--- src/com/codename1/demos/restaurant/MenuDetails.kt
+++ src/com/codename1/demos/restaurant/MenuDetails.kt
@@ -13,7 +13,7 @@ import com.codename1.ui.util.Resources
 fun showMenuDetails(theme: Resources, item: RestaurantMenuItem) {
     val form = Form(BorderLayout())
     form.transitionOutAnimator = CommonTransitions.createFade(800)
-    form.uiid = "MenuScreen"
+    form.responsiveUIId = "MenuScreen"
     form.toolbar.hideToolbar()
     form.isSafeArea = true
     val backButton = Button();
@@ -25,22 +25,22 @@ fun showMenuDetails(theme: Resources, item: RestaurantMenuItem) {
         showMenuScreen(theme)
     }
     val logo = Container().add(theme.getImage("logo.png").scaledHeight(70))
-    logo.uiid = "LogoIcon"
+    logo.responsiveUIId = "LogoIcon"
 
     form.add(NORTH, getToolbar(
-            backButton, Label("Preview".toUpperCase(), "MenuTitleLabel"), logo))
+            backButton, Label("Preview".toUpperCase()).withResponsiveId("MenuTitleLabel"), logo))
 
     var itemDetailsInfo = Container(BorderLayout()).add(CENTER, getMenuDetails(theme, item))
 
     val goBackButton = getDecoratedButton("Go Back".toUpperCase(), theme)
-    goBackButton.uiid = "RedPillButtonFoodDetails"
+    goBackButton.responsiveUIId = "RedPillButtonFoodDetails"
     goBackButton.onClick {
         showMenuScreen(theme)
     }
     itemDetailsInfo.add(SOUTH, goBackButton)
     if (item.imageName != "") {
         val image = Container().add(theme.getImage(item.imageName).scaledWidth(550))
-        image.uiid = "MenuCardImage"
+        image.responsiveUIId = "MenuCardImage"
         itemDetailsInfo = LayeredLayout.encloseIn(itemDetailsInfo, FlowLayout.encloseCenter(image))
     }
     form.add(CENTER, itemDetailsInfo.wrapIntoBorders(theme, bottom = false))
@@ -50,18 +50,24 @@ fun showMenuDetails(theme: Resources, item: RestaurantMenuItem) {
 
 fun getMenuDetails(theme: Resources, item: RestaurantMenuItem): Container {
     val dashboarderContainer = Container(BoxLayout.y());
-    dashboarderContainer.uiid = "ItemDetailsInfo"
+    dashboarderContainer.responsiveUIId = "ItemDetailsInfo"
 
     val foodDetails = Container(BoxLayout.y());
     val priceRow = Container(BoxLayout.xCenter())
     val priceRowContent = Container(BorderLayout(BorderLayout.CENTER_BEHAVIOR_CENTER))
-    priceRowContent.add(CENTER, Label("$5", "ItemPrice"))
+    priceRowContent.add(CENTER, Label("$5").withResponsiveId("ItemPrice"))
     priceRowContent.add(EAST, getGreenLine())
     priceRowContent.add(WEST, getGreenLine())
     priceRow.add(priceRowContent)
     dashboarderContainer.add(priceRow)
-    dashboarderContainer.add(SpanLabel(item.title.toUpperCase(), "FoodDetailsTitle"))
-    dashboarderContainer.add(SpanLabel(item.description, "FoodDetailsDescription"))
+    val title = SpanLabel(item.title.toUpperCase())
+    title.textComponent.responsiveUIId = "FoodDetailsTitle"
+
+    val description = SpanLabel(item.description)
+    description.textComponent.responsiveUIId = "FoodDetailsDescription"
+
+    dashboarderContainer.add(title)
+    dashboarderContainer.add(description)
     foodDetails.add(dashboarderContainer)
     return Container(BoxLayout.y()).add(foodDetails)
 }
